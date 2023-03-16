8bce14d4fa398d6844ff5e0642f92ea7e269c16a
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-03-16 22:28:32 +0100

    Updated.

--- bot_AptosMauToken.js
+++ bot_AptosMauToken.js
@@ -11,13 +11,14 @@ dotenv.config()
 const token = process.env.TELEGRAM_BOT_TOKEN
 const bot = new TelegramBot(token, { polling: true })
 const gifPath = './MauAptos.mp4';
-let chatId = '';
+let chatId = '-1001878116163';
 let nPrevSequenceNumber = -1;
 let bBotStart = false;
 
-bot.onText(/\/mau/, (msg) => {
-  chatId = msg.chat.id;
-});
+// bot.onText(/\/mau/, (msg) => {
+//   chatId = msg.chat.id;
+//   console.log("ChatId", chatId);
+// });
 
 const ExecuteFunction = async () => {
   const url = "https://fullnode.mainnet.aptoslabs.com/v1/accounts/0x2ad8f7e64c7bffcfe94d7dea84c79380942c30e13f1b12c7a89e98df91d0599b/events/0x2ad8f7e64c7bffcfe94d7dea84c79380942c30e13f1b12c7a89e98df91d0599b::swap::PairEventHolder%3C0x1::aptos_coin::AptosCoin,0xf8fa55ff4265fa9586f74d00da4858b8a0d2320bbe94cb0e91bf3a40773eb60::MAU::MAU%3E/swap?limit=100";
